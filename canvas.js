10373716878dc96c94bb98b0fe2933ce498ca7ad
naveen thota <helloitsnaveen@gmail.com>
2020-04-22 12:37:17 -0700

    basic lines and circles

--- canvas.js
+++ canvas.js
@@ -2,4 +2,23 @@ const canvas = document.querySelector('canvas')
 
 canvas.width = window.innerWidth; 
 canvas.height = window.innerHeight;
+
+// returning drawing context to a variable, in this case: 2d 
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
+ctx.fillRect(100, 100, 100, 100);
 console.log(canvas);
+
+// Line 
+ctx.beginPath(); 
+ctx.moveTo(50, 300);
+ctx.lineTo(300, 100); 
+ctx.strokeStyle = "#fa34a3"
+ctx.stroke();
+
+// Arc / Circle 
+ctx.beginPath();
+ctx.arc(300, 300, 30, 0, Math.PI * 2, false)
+ctx.strokeStyle = '#fa32b1'
+ctx.stroke();
